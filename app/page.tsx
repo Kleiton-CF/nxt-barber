052f03211c8c474b21b00b919bd52e26fbd35bb5
Kleiton-CF <kleiton3freitas@gmail.com>
2024-08-07 18:37:14 -0300

    feat: todas alteracoes

--- app/page.tsx
+++ app/page.tsx
@@ -11,6 +11,12 @@ import BarbershopItem from "./_components/barbershop-item"
 
 const Home = async () => {
   const barbershops = await db.barbershop.findMany({})
+  const popularBarbershops = await db.barbershop.findMany({
+    orderBy: {
+      name: "desc",
+    },
+  })
+
   return (
     <div>
       {/* header */}
@@ -71,6 +77,15 @@ const Home = async () => {
         </h2>
         <div className="[&:: -webkit-scrollbar]:hidden flex gap-4 overflow-auto">
           {barbershops.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
+
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Populares
+        </h2>
+        <div className="[&:: -webkit-scrollbar]:hidden flex gap-4 overflow-auto">
+          {popularBarbershops.map((barbershop) => (
             <BarbershopItem key={barbershop.id} barbershop={barbershop} />
           ))}
         </div>
