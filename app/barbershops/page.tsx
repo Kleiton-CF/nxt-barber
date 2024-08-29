8ae6b23b870ca7b8e172dfd0c05642f030a0790a
Kleiton-CF <kleiton3freitas@gmail.com>
2024-08-29 18:37:45 -0300

    feat: ensure barbershops can be searched by title and service

--- app/barbershops/page.tsx
+++ app/barbershops/page.tsx
@@ -17,7 +17,7 @@ const BarbershopsPage = async ({ searchParams }: BarbershopsPageProps) => {
         searchParams?.title
           ? {
               name: {
-                contains: searchParams?.search,
+                contains: searchParams?.title,
                 mode: "insensitive",
               },
             }
@@ -46,7 +46,7 @@ const BarbershopsPage = async ({ searchParams }: BarbershopsPageProps) => {
       </div>
       <div className="px-5">
         <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
-          Resultados para &quot;{searchParams?.title || searchParams?.service}
+          Resultados para, &quot;{searchParams?.title || searchParams?.service}
           &quot;
         </h2>
         <div className="grid grid-cols-2 gap-4">
